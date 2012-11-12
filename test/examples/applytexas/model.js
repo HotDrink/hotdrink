72849a926ca2b16c217dc1b47a1d98d30ea38c61
John Freeman <jfreeman@cse.tamu.edu>
2012-11-12 13:03:34 -0600

    index vs num in applytx

--- test/examples/applytexas/model.js
+++ test/examples/applytexas/model.js
@@ -8,7 +8,7 @@ var LEVELS = [
   "NATIONAL"
 ];
 
-var ActivityYear = hd.model(function ActivityYear(year, index) {
+var ActivityYear = hd.model(function ActivityYear(year, num) {
   this.isActive  = hd.variable(false);
   this.year      = year;
   this.position  = hd.variable("");
@@ -16,19 +16,22 @@ var ActivityYear = hd.model(function ActivityYear(year, index) {
   this.hours     = hd.variable(0);
   this.weeks     = hd.variable(0);
   this.id        = hd.computed(function () {
-    return year + index();
+    return year + num();
   });
 });
 
 var Activity = hd.model(function Activity(list) {
   this.index = hd.computed(function () {
-    return list().indexOf(this) + 1;
+    return list().indexOf(this);
+  });
+  this.num  = hd.computed(function () {
+    return this.index() + 1;
   });
   this.name  = hd.variable("");
   this.level = hd.variable();
   this.description = hd.variable("");
   this.years = ["Fresh", "Soph", "Junior", "Senior"].map(function(year) {
-    return new ActivityYear(year, this.index);
+    return new ActivityYear(year, this.num);
   }, this);
 });
 
@@ -39,18 +42,18 @@ var Model = hd.model(function Model() {
 });
 
 Model.prototype.promote = function promote(act) {
-  this.activities.swap(act.index() - 1, act.index() - 2);
+  this.activities.swap(act.index(), act.index() - 1);
 };
 
 Model.prototype.demote = function demote(act) {
-  this.activities.swap(act.index() - 1, act.index());
+  this.activities.swap(act.index(), act.index() + 1);
 };
 
 Model.prototype.addBefore = function addBefore(act) {
-  this.activities.splice(act.index() - 1, 0, new Activity(this.activities));
+  this.activities.splice(act.index(), 0, new Activity(this.activities));
 }
 
 Model.prototype.addAfter = function addAfter(act) {
-  this.activities.splice(act.index(), 0, new Activity(this.activities));
+  this.activities.splice(act.index() + 1, 0, new Activity(this.activities));
 }
 

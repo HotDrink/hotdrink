57676f40747d1da21ec0fbcc8363e25e0399edbd
John Freeman <jfreeman@cse.tamu.edu>
2012-11-28 11:28:16 -0600

    link to external CSS before external scripts

--- tutorial/template.php
+++ tutorial/template.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <title>Tutorial | HotDrink</title>
+  <link rel="stylesheet" href="main.css"/>
   <script type="text/javascript" src="https://raw.github.com/kriskowal/es5-shim/master/es5-shim.min.js"></script>
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <?php if ($DEBUG) { ?>
@@ -8,8 +10,6 @@
 <?php } else { ?>
   <script type="text/javascript" src="/hotdrink.js"></script>
 <?php } ?>
-  <link rel="stylesheet" href="main.css"/>
-  <title>Tutorial | HotDrink</title>
   <script type="text/javascript">
 
 <?= $model ?>

0d1b3d35c96ccfc2d6462ad636a2bd7945ca0109
Gabriel Foust <gfoust@cse.tamu.edu>
2015-08-08 13:20:42 -0500

    WIP: array1

--- src/hd/model/path.ts
+++ src/hd/model/path.ts
@@ -9,6 +9,30 @@ module hd.model {
   export
   type Position = number;
 
+  export
+  type PositionSet = u.ArraySet<number>;
+
+  export
+  function addPosition( set: PositionSet, pos: Position ) {
+    if (set === null || pos === null) { return null; }
+    return u.arraySet.add( set, pos );
+  }
+
+  export
+  function hasPosition( set: PositionSet, pos: Position ) {
+    if (set === null) { return true; }
+    if (pos === null) { return false; }
+    return u.arraySet.contains( set, pos );
+  }
+
+  export union( target: PositionSet, set: PositionSet ) {
+    if (target === null || set === null) { return null; }
+    for (var i = 0, l = set.length; i < l; ++i) {
+      u.arraySet.add( target, set[i] );
+    }
+    return target;
+  }
+
   /*==================================================================
    * Pattern for an array index in a path
    */
@@ -24,7 +48,7 @@ module hd.model {
     }
 
     // Get the result for this pattern at a particular position
-    apply( pos: Position ) {
+    apply( pos: Position ): Position {
       if (this.scale == 0) {
         return this.offset;
       }
@@ -35,7 +59,7 @@ module hd.model {
 
     // Inverse of apply: what value of index variable would
     //   result in the value for that variable in given position?
-    inverse( pos: Position ) {
+    inverse( pos: Position ): Position {
       if (this.scale == 0) { return null; }
       var i = (pos - this.offset) / this.scale;
       if (Math.floor( i ) === i) {
@@ -73,13 +97,19 @@ module hd.model {
     start: Context;
 
     // The property path to follow
-    path: string[];
+    legs: (string|IndexPattern)[];
+
+    // Maximum index variable used
+    private cardinality: number;
+
+    // Doesn't subscribe to any properties or arrays
+    private constant = true;
 
     // Any observable properties subscribed to along the way
     observables0: r.ProxyObservable<any>[] = null;
     observables1: r.ProxyObservable<any>[][] = null;
 
-    // The result at the end of the path
+    // The result (or array of results) at the end of the path
     result: any;
 
     /*----------------------------------------------------------------
@@ -88,71 +118,85 @@ module hd.model {
     constructor( start: Context, path: string ) {
       super();
       this.start = start;
+
+      // Break path down into property names and index patterns
+      var legs = parse( path );
+      var cardinality = -1;
+      if (legs) {
+        cardinality = calcCardinality( path, legs );
+      }
+      if (legs && cardinality >= 0) {
+        this.legs = legs;
+        this.cardinality = cardinality;
+      }
+      else {
+        // Create a path which always just returns undefined
+        this.start = undefined;
+        this.legs = [];
+        this.cardinality = 0;
+      }
+
+      if (cardinality == 1) {
+        this.result = [];
+      }
+
       this.path = path.split( '.' );
       this.followPath();
-    }
-
-    /*----------------------------------------------------------------
-     * The current result for the path serach
-     */
-    get(): any {
-      return this.result;
     }
 
     /*----------------------------------------------------------------
      * Were there any properties in the path?
      */
     isConstant(): boolean {
-      return ! this.observables0 && ! this.observables1;
+      return this.constant;
     }
 
     /*----------------------------------------------------------------
-     * Subscribe and receive the first value.
+     * The current result for the path serach
      */
-    addObserver( observer: r.Observer<any> ): r.Observer<any>;
-    addObserver( object: Object,
-                 onNext: (value: any) => void,
-                 onError: (error: any) => void,
-                 onCompleted: () => void        ): r.Observer<any>;
-    addObserver<U>( object: Object,
-                    onNext: (value: any, id?: U) => void,
-                    onError: (error: any, id?: U) => void,
-                    onCompleted: (id?: U) => void,
-                    id: U                                  ): r.Observer<any>;
-    addObserver( object: Object,
-                 onNext?: (value: any, id?: any) => void,
-                 onError?: (error: any, id?: any) => void,
-                 onCompleted?: (id?: any) => void,
-                 id?: any                                  ): r.Observer<any> {
-      if (this.isConstant()) {
-        if (arguments.length == 1) {
-          (<r.Observer<any>>object).onCompleted();
+    get( pos: Position ): any {
+      if (this.cardinality == 0 && pos === null) {
+        return this.result;
+      }
+      if (this.cardinality == 1 && pos !== null) {
+        return this.result[pos];
+      }
+    }
+
+    /*----------------------------------------------------------------
+     */
+    private
+    addResult( pos: Position, val: any ) {
+      if (this.cardinality == 0 && pos === null) {
+        this.result = val;
+      }
+      else if (this.cardinality == 1 && pos !== null) {
+        this.result[pos] = val;
+      }
+    }
+
+    /*----------------------------------------------------------------
+     */
+    private
+    addObservable( pos: Position, vable: ProxyObservable ) {
+      if (pos === null) {
+        if (! this.observables0) {
+          this.observables0 = [vable];
         }
         else {
-          onCompleted.call( object, id );
+          this.observables0.push( vable );
         }
       }
       else {
-        var added: r.Observer<any>;
-        if (arguments.length === 1) {
-          added = super.addObserver( <r.Observer<any>>object );
+        if (! this.observables1) {
+          this.observables1 = [];
+        }
+        if (! this.observables1[pos]) {
+          this.observables1[pos] = [vable];
         }
         else {
-          added = super.addObserver( object, onNext, onError, onCompleted, id );
+          this.observables1[pos].push( vable );
         }
-        return added;
-      }
-    }
-
-    /*----------------------------------------------------------------
-     * Indicates that this path is no longer needed
-     */
-    cancel() {
-      if (! this.isConstant()) {
-        this.observables0.forEach( function( p: r.ProxySignal<any> ) {
-          p.removeObserver( this );
-        }, this );
-        this.observables0 = null;
       }
     }
 
@@ -160,67 +204,128 @@ module hd.model {
      * Perform the search, subscribing to any properties encountered.
      */
     private
-    followPath() {
-      var properties: r.ProxySignal<any>[] = [];
-      var m = this.start;
-      for (var i = 0, l = this.path.length; typeof m === 'object' && m !== null && i < l; ++i) {
-        var name = this.path[i];
-        var propname = '$' + name;
-        if (propname in m) {
-          var p = m[propname];
-          p.addObserverChangesOnly( this );
-          properties.push( p );
-          m = p.get();
+    followPath( ctx: Context, i: number; pos: Position ) {
+
+      for (var l = this.legs.length; i < l && ctx instanceof Context; ++i) {
+        var leg = this.legs[i];
+        if (typeof leg === 'string') {
+          var propname = '$' + leg;
+          if (propname in ctx) {
+            var p = ctx[propname];
+            p.addObserverChangesOnly( this, this.onNextProperty, null, null, pos );
+            this.addObservable( pos, p );
+            ctx = p.get();
+          }
+          else {
+            ctx = ctx[leg];
+          }
         }
-        else {
-          m = m[name];
+        else if (leg instanceof IndexPattern && ctx instanceof ArrayContext) {
+          if (leg.scale == 0 || pos !== null) {
+            var idx = leg.apply( pos );
+            ctx.addObserver( this, this.onNextArray, null, null, {ctx: ctx, legi: i, pos: pos} );
+            ctx = ctx[idx];
+          }
+          else {
+            ctx.addObserver( this, this.onNextArray, null, null, {ctx: ctx, legi: i, pos: pos} );
+            for (var j = 0, m = ctx.length; j < m; ++j) {
+              if (ctx[j] !== undefined) {
+                var n = leg.inverse( j );
+                if (n !== null) {
+                  this.followPath( ctx[j], i + 1, n );
+                }
+              }
+            }
+            break; // we followed the rest recursively
+          }
         }
       }
-      if (i < l) {
-        m = undefined;
+
+      if (i == l && ctx !== undefined) {
+        this.addResult( pos, ctx );
       }
-      if (m !== this.result) {
-        this.result = m;
-        this.sendNext( m );
+    }
+
+    /*----------------------------------------------------------------
+     */
+    private followOnePath( pos: Position ) {
+      var ctx = this.start;
+      for (var i = 0, l = this.legs.length; i < l && ctx instanceof Context; ++i) {
+        var leg = this.legs[i];
+        if (typeof leg === 'string') {
+          ctx = ctx[leg];
+        }
+        else if (leg instanceof IndexPattern && ctx instanceof ArrayContext) {
+          if (leg.scale == 0) {
+            var idx = leg.apply( pos );
+            ctx = ctx[idx];
+          }
+          else {
+            var n = leg.inverse( j );
+            if (n !== nul) {
+              this.followPath( ctx[j], i + 1, n );
+            }
+            break;
+          }
+        }
       }
-      if (properties.length > 0) {
-        this.observables0 = properties;
+    }
+
+    /*----------------------------------------------------------------
+     * Indicates that this path is no longer needed
+     */
+    cancel( pos: Position ) {
+      if (pos === null) {
+        if (this.observables1) {
+          this.observables1.forEach( this.unsubscribeAll, this );
+          this.observables1 = null;
+        }
+        this.unsubscribeAll( this.observables0 );
+        this.observables0 = null;
       }
       else {
-        // should be unnecessary, but just in case
-        this.observables0 = null;
-        this.sendCompleted();
+        if (this.observables1[pos]) {
+          this.unsubscribeAll( this.observables1[pos] );
+          this.observables1[pos] = null;
+        }
+      }
+    }
+
+    private
+    unsubscribeAll( vables: ProxyObservable[] ) {
+      if (vables) {
+        vables.forEach( this.unsubsribe, this );
+      }
+    }
+
+    private
+    unsubscribe( vable: ProxyObservable ) {
+      if (vable) {
+        vable.removeObserver( this );
       }
     }
 
     /*----------------------------------------------------------------
-     * One of the propeties on which the result depends has changed.
-     * Unsubscribe from all properties and redo search.
      */
-    private
-    onNext() {
-      this.cancel();
-      this.followPath();
+    onNextProperty( val: any, pos: Position ) {
+      this.cancel( p.pos );
+      if (pos == null) {
+        this.followPath( this.start, 0, null );
+      }
+      else {
+        this.followOnePath( pos );
+      }
+      this.sendNext( pos );
     }
 
     /*----------------------------------------------------------------
-     * Should not occur; but if it does, treat it like a property
-     * change.
      */
-    private
-    onError() {
-      this.cancel();
-      this.followPath();
-    }
 
-    /*----------------------------------------------------------------
-     * Should not occur; but if it does, treat it like a property
-     * change.
-     */
-    private
-    onCompleted() {
-      this.cancel();
-      this.followPath();
+    onNextArray( idx: number, p: {ctx: Context, legi: number, pos: Position} ) {
+      var leg = this.legs[p.legi];
+      var n = leg.inverse( idx );
+      if (n !== null && (pos === null || pos === n) {
+      }
     }
   }
 
